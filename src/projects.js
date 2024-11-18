5351dbaad543e8fb46e11e2e0986dd5770745505
jyy009 <153378401+jyy009@users.noreply.github.com>
2024-11-18 16:47:45 +0100

    Display project links on projects page

--- src/projects.js
+++ src/projects.js
@@ -4,10 +4,32 @@ import "./projects.css";
 export const projects = () => {
   const projectsSection = document.getElementById("projects-section");
 
+  const projectsInfoTemplate = document.getElementById(
+    "projects-info-template"
+  );
+  const cloneProjectsInfoTemplate =
+    projectsInfoTemplate.content.cloneNode(true);
+  projectsSection.appendChild(cloneProjectsInfoTemplate);
+
   const renderProject = () => {
-    const projectsTemplate = document.getElementById("projects-template");
-    const cloneProjectsTemplate = projectsTemplate.content.cloneNode(true);
-    projectsSection.appendChild(cloneProjectsTemplate);
+    const projectsLinkTemplate = document.getElementById(
+      "projects-link-template"
+    );
+
+    projectsJson.forEach((proj) => {
+      const cloneProjectsLinkTemplate =
+        projectsLinkTemplate.content.cloneNode(true);
+
+      const projectTitle =
+        cloneProjectsLinkTemplate.querySelector(".proj-title");
+      projectTitle.textContent = proj.title;
+
+      const projectAuthor =
+        cloneProjectsLinkTemplate.querySelector(".proj-author");
+      projectAuthor.textContent = `- ${proj.author}`;
+
+      projectsSection.appendChild(cloneProjectsLinkTemplate);
+    });
   };
 
   renderProject();
