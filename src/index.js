8c8727f1fe1f7f3ed2020b1d33067046623c1458
ffups <142579057+ffups@users.noreply.github.com>
2024-11-20 09:22:56 +0100

    feat: add Babel configuration and implement template rendering with dynamic data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import "./styles.css";
 
-
-console.log("hi" + "there");
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('container');
+    const template = document.getElementById('my-template').content;
+  
+    const data = [
+      { name: 'WebFontFre', description: 'Description for John' },
+      { name: 'Jane Smith', description: 'Description for Jane' },
+      { name: 'Alice Johnson', description: 'Description for Alice' },
+      { name: 'Bob Brown', description: 'Description for Bob'},
+    ];
+  
+    data.forEach(item => {
+      const clone = document.importNode(template, true);
+      clone.querySelector('.item-title').textContent = item.name;
+      clone.querySelector('.item-description').textContent = item.description;
+      container.appendChild(clone);
+    });
+  });
--- webpack.common.js
+++ webpack.common.js
@@ -19,6 +19,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
@@ -29,6 +36,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
