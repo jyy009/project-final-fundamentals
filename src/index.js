c62037107497bf0cf0dc39c6c8c05e36172d2b4d
ffups <142579057+ffups@users.noreply.github.com>
2024-11-20 19:31:29 +0100

    feat: changed the styling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // Add loaded class to body after a delay to trigger the transition
     setTimeout(() => {
       document.body.classList.add('loaded');
-    }, 100  ); // Adjust the delay as needed
+    }, 500  ); 
 
   });
